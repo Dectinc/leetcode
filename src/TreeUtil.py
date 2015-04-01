b1c33bdcd0c3a389d19e4ae67f4adaf48e212ad0
Dectinc <chensj009@gmail.com>
2015-04-01 16:28:41 +0800

    add TreeUtil

--- src/TreeUtil.py
+++ src/TreeUtil.py
@@ -12,6 +12,70 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def inorderTraversal(self):
+        nodes = []
+        traversal = []
+        pointer = self
+        while pointer is not None:
+            nodes.append(pointer)
+            if pointer.left is not None:
+                pointer = pointer.left
+            else:
+                try:
+                    pointer = nodes.pop()
+                    traversal.append(pointer.val)
+                    while pointer.right is None:
+                        pointer = nodes.pop()
+                        traversal.append(pointer.val)
+                    pointer = pointer.right
+                except:
+                    break
+        return traversal
+
+    def preorderTraversal(self):
+        nodes = []
+        traversal = []
+        pointer = self
+        while pointer is not None:
+            if pointer.right is not None:
+                nodes.insert(0, pointer.right)
+            traversal.append(pointer.val)
+            if pointer.left is not None:
+                pointer = pointer.left
+            else:
+                if len(nodes) == 0:
+                    break
+                pointer = nodes.pop(0)
+        return traversal
+
+    def postorderTraversal(self):
+        nodes = []
+        traversal = []
+        pointer = self
+        while pointer is not None:
+            pointer.visited = False
+            nodes.append(pointer)
+            if pointer.left is not None:
+                pointer = pointer.left
+            elif len(nodes) == 0:
+                break
+            else:
+                try:
+                    pointer = nodes.pop()
+                    if pointer.visited:
+                        traversal.append(pointer.val)
+                        pointer = nodes.pop()
+                    else:
+                        while pointer.right is None or pointer.visited:
+                            traversal.append(pointer.val)
+                            pointer = nodes.pop()
+                        pointer.visited = True
+                        nodes.append(pointer)
+                        pointer = pointer.right
+                except:
+                    break
+        return traversal
+
 
 if __name__ == '__main__':
     sol = Solution()
