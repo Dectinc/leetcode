4013733b57802dab8372e20885ec6e46b5887504
Dectinc <chensj009@gmail.com>
2015-04-01 15:52:02 +0800

    add TreeUtil

---
+++ src/TreeUtil.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-04-01 12:54:28
+# @Author  : chenshijiang (i@dectinc.cc)
+# @Link    : http://dectinc.cc
+# @Version : 0.0.1
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+if __name__ == '__main__':
+    sol = Solution()
+
