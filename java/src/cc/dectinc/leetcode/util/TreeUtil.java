81e9ce9acae62b91e2a54b077f95681d6a42f929
Dectinc <chensj009@gmail.com>
2015-04-02 17:19:06 +0800

    add Utils

---
+++ java/src/cc/dectinc/leetcode/util/TreeUtil.java
@@ -0,0 +1,13 @@
+package cc.dectinc.leetcode.util;
+
+/**
+ * @author Dectinc (i@dectinc.cc)
+ * @date 2015-04-02 17:19 PM
+ */
+public class TreeUtil {
+    public TreeUtil() {
+    }
+
+    public static void main(String args[]) {
+    }
+}
