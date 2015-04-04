a41136be37e78ae2a7365ba31f08ca6507c2825c
Dectinc <chensj009@gmail.com>
2015-04-04 23:55:11 +0800

    add TreeLinkNode

---
+++ java/src/cc/dectinc/leetcode/structs/TreeLinkNode.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package cc.dectinc.leetcode.structs;
+
+/**
+ * @author Dectinc
+ * @version Apr 4, 2015 11:54:27 PM
+ * 
+ */
+public class TreeLinkNode {
+	int val;
+	TreeLinkNode left, right, next;
+
+	TreeLinkNode(int x) {
+		val = x;
+	}
+}
