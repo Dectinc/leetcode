4edbd85b35fa55ac82b8725e1dfaee1eef763b46
Dectinc <chensj009@gmail.com>
2015-04-13 23:43:40 +0800

    add WordBreak

---
+++ java/src/cc/dectinc/leetcode/WordBreak.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package cc.dectinc.leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Dectinc
+ * @version Apr 13, 2015 9:43:48 PM
+ * 
+ */
+public class WordBreak {
+	private int maxLength = -1;
+
+	public boolean wordBreak(String s, Set<String> dict) {
+		if (dict.size() == 0) {
+			return false;
+		}
+		maxLength = getMaxLength(dict);
+		return helper(s, dict);
+	}
+
+	private boolean helper(String s, Set<String> dict) {
+		if (s.isEmpty() || dict.contains(s)) {
+			return true;
+		}
+		int length = Math.min(s.length(), maxLength);
+		for (int i = 1; i <= length; i++) {
+			if (dict.contains(s.subSequence(0, i))
+					&& helper(s.substring(i), dict)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private int getMaxLength(Set<String> dict) {
+		int length = 0;
+		for (String string : dict) {
+			length = Math.max(length, string.length());
+		}
+		return length;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		WordBreak sol = new WordBreak();
+		String s = "aaaaaaaaaaaaaaaaaab";
+		Set<String> dict = new HashSet<String>();
+		String[] strings = { "a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa",
+				"aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa" };
+		dict.addAll(Arrays.asList(strings));
+		System.out.println(sol.wordBreak(s, dict));
+	}
+
+}
