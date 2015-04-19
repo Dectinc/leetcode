733658a967184db0856346d8b95b61fa9638c084
Dectinc <chensj009@gmail.com>
2015-04-19 22:49:47 +0800

    Revert "tmp EvaluateReversePolishNotation"
    This reverts commit c61074b23bb00f7618e58574a95c7ee42bcb73ce.

--- java/src/cc/dectinc/leetcode/EvaluateReversePolishNotation.java
+++ java/src/cc/dectinc/leetcode/EvaluateReversePolishNotation.java
@@ -16,6 +16,7 @@ import java.util.Stack;
  * 
  */
 public class EvaluateReversePolishNotation {
+
 	public int evalRPN(List<String> tokens) {
 		String[] operatorStrings = { "+", "-", "*", "/" };
 		Set<String> operators = new HashSet<String>();
