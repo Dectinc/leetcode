3e7b093a2d485f70dd0f3f5d41c4a148adbfcec4
Dectinc <chensj009@gmail.com>
2015-09-19 13:50:50 +0800

    add BSTIterator

--- java/src/cc/dectinc/leetcode/BSTIterator.java
+++ java/src/cc/dectinc/leetcode/BSTIterator.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package cc.dectinc.leetcode;
 
@@ -10,41 +10,44 @@ import java.util.Stack;
 /**
  * @author Dectinc
  * @version Apr 20, 2015 9:12:30 PM
- * 
  */
 public class BSTIterator {
-	Stack<TreeNode> stack;
+    Stack<TreeNode> stack;
 
-	public BSTIterator(TreeNode root) {
-		stack = new Stack<TreeNode>();
-		allLeftIntoStack(root);
-	}
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<TreeNode>();
+        allLeftIntoStack(root);
+    }
 
-	/** @return whether we have a next smallest number */
-	public boolean hasNext() {
-		return stack.size() > 0;
-	}
+    /**
+     * @return whether we have a next smallest number
+     */
+    public boolean hasNext() {
+        return stack.size() > 0;
+    }
 
-	/** @return the next smallest number */
-	public int next() {
-		TreeNode node = stack.pop();
-		allLeftIntoStack(node.right);
-		return node.val;
-	}
+    /**
+     * @return the next smallest number
+     */
+    public int next() {
+        TreeNode node = stack.pop();
+        allLeftIntoStack(node.right);
+        return node.val;
+    }
 
-	private void allLeftIntoStack(TreeNode node) {
-		while (node != null) {
-			stack.add(node);
-			node = node.left;
-		}
-	}
+    private void allLeftIntoStack(TreeNode node) {
+        while (node != null) {
+            stack.add(node);
+            node = node.left;
+        }
+    }
 
-	public static void main(String[] args) {
-		TreeNode root = TreeNode.constructTreeWithLevelTraversal(new Integer[] {
-				2, 1 });
-		BSTIterator iter = new BSTIterator(root);
-		while (iter.hasNext()) {
-			System.out.print("" + iter.next() + "\t");
-		}
-	}
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.constructTreeWithLevelTraversal(new Integer[]{
+                2, 1});
+        BSTIterator iter = new BSTIterator(root);
+        while (iter.hasNext()) {
+            System.out.print("" + iter.next() + "\t");
+        }
+    }
 }
